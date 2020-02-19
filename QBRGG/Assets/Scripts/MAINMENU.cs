ec4758a6042338d980faef55e2cedef743cd7fcb
jonanobonano <c16743131>
2020-02-19 06:30:03 +0000

    main menu

---
+++ QBRGG/Assets/Scripts/MAINMENU.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MAINMENU : MonoBehaviour
+{
+
+    public void LoadLevel()
+    {
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 0);
+        StartCoroutine(LetTheBellRing());
+    }
+
+    IEnumerator LetTheBellRing()
+    {
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene("_Main");
+    }
+
+}
